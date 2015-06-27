33a8f25c7f394422ac20c171efb9426c096a4ec1
Elaine <elainegn@mit.edu>
2015-06-27 16:59:05 -0400

    restructuring

--- CanRead/routes/index.js
+++ CanRead/routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/upload', function(req, res){
+	res.render('upload_content');
+});
+
+router.get('/login_success', function(req, res){
+	res.render('login_sucess');
+})
+router.get('/spritzer', function(req, res){
+	res.render('lesson07');
+})
 
 module.exports = router;
