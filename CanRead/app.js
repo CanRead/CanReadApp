9a7314c1b2f9c6a0b2087f8483a716bb2fa3dcc3
Elaine <elainegn@mit.edu>
2015-06-27 14:44:20 -0400

    additional dependencies

--- CanRead/app.js
+++ CanRead/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,6 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'cAn r3aD R@nd0m$tr', saveUninitialized: true, resave: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
