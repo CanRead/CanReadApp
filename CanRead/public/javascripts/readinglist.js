c605215ee63b55433f105fc7fba5077490478835
Elaine <elainegn@mit.edu>
2015-06-27 23:55:59 -0400

    downloads

--- CanRead/public/javascripts/readinglist.js
+++ CanRead/public/javascripts/readinglist.js
@@ -3,13 +3,14 @@ var apikey = "685e16af4b72e10a6cf15409ad0d979a";
 $(document).ready(function(){
 	var sif_token = $('#sif_token').attr('value');
 	console.log('sif_token', sif_token);
+	// var d = new FormData();
+	// d.append('id', '4923ccbd-2a4c-454a-aa49-0d12c1b1d93b');
 
 	$.ajax({
 		  method: "GET",
-		  url: "http://sandbox.api.hmhco.com/v1/documents",
+		  url: "http://sandbox.api.hmhco.com/v1/documents/4923ccbd-2a4c-454a-aa49-0d12c1b1d93b",
 		  processData: false, 
-		  contentType: false,
-		  data: formData, 
+		  // data: {id: '4923ccbd-2a4c-454a-aa49-0d12c1b1d93b'},
 		  beforeSend: function (request)
 		            {
 		                request.setRequestHeader("Vnd-HMH-Api-Key", apikey);
@@ -17,7 +18,6 @@ $(document).ready(function(){
 		            },
 		  success: function(response) {
 		  	console.log(response);
-		  	$('#load').innerHTML(response);
 	  		}
 	});
 });
